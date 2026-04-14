b5bf230077d8849c5a0be3d7452c9b192d76c4a6
Ayush Raj <ayushraj@Ayushs-MacBook-Air.local>
2026-04-14 23:46:33 +0530

    environment Setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from .env file
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
